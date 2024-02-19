87279264d932463d07818952aaaaa1aede704c4d
Siva121195 <160484700+Siva121195@users.noreply.github.com>
2024-02-19 16:10:12 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+/*Escape Sequence*/
+{
+    printf("David says,\"Programming is fun\"\n");
+    printf("**conditions apply,\"offer valid until tomorrow\"\n");
+    printf("C:\\My computer\\My Folder\n");
+    printf("D:/My documents/My File\n");
+    printf("\\ \\ \\ \\Today is holiday\\ \\ \\ \\\n");
+    printf("This is a triple quoted string\"\"\"This month has 30 days\"\"\"");
+}
